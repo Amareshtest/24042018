9e7058c2542ebfa89d0075a985dfdc480a476769
Amaresh <Amaresh@192.168.0.3>
2018-04-24 22:18:18 +0530

    first checkin to local repository

---
+++ deimo/src/main/java/com/home/deimo/App.java
@@ -0,0 +1,13 @@
+package com.home.deimo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ deimo/src/main/java/com/home/deimo/ftc.java
@@ -0,0 +1,10 @@
+package com.home.deimo;
+
+public class ftc {
+
+	public static void main(String[] args) {
+System.out.println("Test123");
+
+	}
+
+}
